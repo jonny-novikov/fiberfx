67348fcdfa3188165bf19ca239e339b8329c1246
Ivan Novikov <jonny.novikov@gmail.com>
2026-06-30 18:06:51 +0300

    [cm-game] codemojex cleanup

--- mercury/codemojex/packages/dto/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./common.js";
-export * as Players from "./players.js";
-export * as Rooms from "./rooms.js";
-export * as Games from "./games.js";
-export * as Guesses from "./guesses.js";
-export * as EmojiSets from "./emoji-sets.js";
-export * as Transactions from "./transactions.js";
--- mercury/codemojex/packages/types/src/index.ts
+++ mercury/codemojex/packages/types/src/index.ts
@@ -1,2 +1,1 @@
-export * from "./branded.js";
 export * from "./enums.js";
